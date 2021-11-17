faa01c2e2295400e5082c509ae201187377f1a73
Agustin Mulet <agustin.mulet@gmail.com>
2021-11-17 00:15:11 -0300

    Housekeeping deps, Gatsby v4, componente Image nuevo, cleanup

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
-<!-- AUTO-GENERATED-CONTENT:START (STARTER) -->
-<p align="center">
-  <a href="https://www.gatsbyjs.org">
-    <img alt="Gatsby" src="https://www.gatsbyjs.org/monogram.svg" width="60" />
-  </a>
-</p>
+![Logo de Gatsby](/src/images/gatsby-icon.png)
 <h1 align="center">
   Workshop de Gatsby
 </h1>
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,42 +1,38 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+import { GatsbyImage } from "gatsby-plugin-image";
 
 /*
- * This component is built using `gatsby-image` to automatically serve optimized
+ * This component is built using `gatsby-plugin-image` to automatically serve optimized
  * images with lazy loading and reduced file sizes. The image is loaded using a
  * `StaticQuery`, which allows us to load the image from directly within this
  * component, rather than having to pass the image data down from pages.
  *
  * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.app/gatsby-image
- * - `StaticQuery`: https://gatsby.app/staticquery
+ * - `gatsby-plugin-image`: https://www.gatsbyjs.com/docs/how-to/images-and-media/using-gatsby-plugin-image/
+ * - `StaticQuery`: https://www.gatsbyjs.com/docs/how-to/querying-data/static-query/
  */
 
-const Image = ({ imgName, style }) => (
+const Image = ({ imgName, alt }) => (
   <StaticQuery
     query={graphql`
       query {
         allImageSharp {
-          edges {
-            node {
-              fluid(maxWidth: 960) {
-                ...GatsbyImageSharpFluid
-                originalName
-              }
-            }
+          nodes {
+            gatsbyImageData
           }
         }
       }
     `}
-    render={data => {
-      const image = data.allImageSharp.edges.find(
-        edge => edge.node.fluid.originalName === imgName,
+    render={(data) => {
+      const image = data.allImageSharp.nodes.find(
+        (node) => node.gatsbyImageData.images.fallback.src.includes(imgName),
       )
       if (!image) {
         return null
       }
-      return <Img fluid={image.node.fluid} style={style} />
+      console.log({image})
+      return <GatsbyImage image={image.gatsbyImageData} alt={alt} placeholder="blurred" />;
     }}
   />
 )
